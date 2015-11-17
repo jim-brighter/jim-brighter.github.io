d8c62ad33140d13e858b087f0dd58db795c535a5
Jim Brighter <jbrighter92@gmail.com>
2015-11-16 20:23:56 -0500

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# jim-brighter.github.io
+mygithubpage
+============
+
+to-do:
+- add home page with links to twitter, github, resume, email -- DONE
+- make things fancy -- DONE
+- add some JavaScript animations
+- add more content?
---
+++ js/math.js
@@ -0,0 +1,64 @@
+$(document).ready(function() {
+    alert("ONLY WORKS WITH SIMPLE CALCULATIONS. MAKE SURE YOU CLEAR AFTER EACH CALCULATION. DOESN'T SUPPORT NEGATIVE NUMBERS AS INPUTS.");
+
+    var num1 = "";
+    var num2 = "";
+    var ops = "";
+    var result = "";
+
+    $(".number").click(function() {
+        var n = $(this).text();
+        $("#display-text").text($("#display-text").text() + n);
+    });
+
+    $(".operator").click(function() {
+        ops = $(this).text();
+        num1 = $("#display-text").text();
+        $("#display-text").text("");
+    });
+
+    $("#clear").click(function() {
+        num1 = "";
+        num2 = "";
+        ops = "";
+        result = "";
+        $("#display-text").text("");
+    });
+
+    $("#equals").click(function() {
+        num2 = $("#display-text").text();
+        switch (ops) {
+            case "/":
+            if (Number(num2) != 0) {
+                result = Number(num1) / Number(num2);
+            } else {
+                result = 0;
+            }
+            break;
+
+            case "*":
+            result = Number(num1) * Number(num2);
+            break;
+
+            case "+":
+            result = Number(num1) + Number(num2);
+            break;
+
+            case "-":
+            result = Number(num1) - Number(num2);
+            break;
+
+            default:
+            result = 0;
+            break;
+        }
+
+        result = Math.floor(result * 1000) / 1000;
+
+        $("#display-text").text("" + result);
+        num1 = "";
+        num2 = "";
+        ops = "";
+        result = "";
+    });
+});
